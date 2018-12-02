ada7100a85fdce450721384d7e98b8979bbed98b
April Domingo <aprildomingo3@gmail.com>
2018-12-01 21:34:04 -0500

    FinalCommit

--- Assets/Scripts/APDOPlayer_Controller.cs
+++ Assets/Scripts/APDOPlayer_Controller.cs
@@ -3,77 +3,93 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
 
-public class APDOPlayer_Controller : MonoBehaviour {
+public class APDOPlayer_Controller : MonoBehaviour
+{
 
-    public float speed;             //Floating point variable to store the player's movement speed.
+    public float speed;   
     public Text countText;
     public Text winText;
     public AudioSource fishCollect;
+    public AudioSource animalSaved;
 
-    private Rigidbody2D rb2d;       //Store a reference to the Rigidbody2D component required to use 2D Physics.
+    private Rigidbody2D rb2d;  
     private int count;
 
+    private float timer;
 
-    // Use this for initialization
+    public Text endText;
+
+
     void Start()
     {
-        //Get and store a reference to the Rigidbody2D component so that we can access it.
         rb2d = GetComponent<Rigidbody2D>();
         fishCollect = GetComponent<AudioSource>();
+        animalSaved = GetComponent<AudioSource>();
+
         count = 0;
         winText.text = "";
-        SetCountText ();
+        endText.text = "";
+        SetCountText();
     }
 
-    //FixedUpdate is called at a fixed interval and is independent of frame rate. Put physics code here.
     void FixedUpdate()
     {
-        //Store the current horizontal input in the float moveHorizontal.
         float moveHorizontal = Input.GetAxis("Horizontal");
 
-        //Store the current vertical input in the float moveVertical.
         float moveVertical = Input.GetAxis("Vertical");
 
-        //Use the two store floats to create a new Vector2 variable movement.
         Vector2 movement = new Vector2(moveHorizontal, moveVertical);
 
-        //Call the AddForce function of our Rigidbody2D rb2d supplying movement multiplied by speed to move our player.
         rb2d.AddForce(movement * speed);
 
         //transform.rotation = Quaternion.Euler(0, 0, Mathf.Atan2(movement.y - transform.position.y, movement.x - transform.position.x) * Mathf.Rad2Deg - 90);
 
+        timer = timer + Time.deltaTime;
+        if (timer >= 10)
+        {
+            endText.text = "You Lose!";
+            StartCoroutine(ByeAfterDelay(2));
+
+        }
+
     }
 
-    //OnTriggerEnter2D is called whenever this object overlaps with a trigger collider.
     void OnTriggerEnter2D(Collider2D other)
     {
-        //Check the provided Collider2D parameter other to see if it is tagged "PickUp", if it is...
         if (other.gameObject.CompareTag("FishCollect"))
         {
             other.gameObject.SetActive(false);
             fishCollect.Play();
             count = count + 1;
-            SetCountText ();
+            //GameLoader.AddScore(1);
+            SetCountText();
         }
 
         if (other.gameObject.CompareTag("AnimalSave"))
         {
             other.gameObject.SetActive(false);
+            animalSaved.Play();
             count = count + 5;
-            SetCountText ();
+            SetCountText();
         }
     }
 
     void SetCountText()
     {
-        countText.text = "Count: " + count.ToString ();
+        countText.text = "Count: " + count.ToString();
         if (count >= 10)
         {
             winText.text = "You Saved The Animal!";
+            StartCoroutine(ByeAfterDelay(2));
 
         }
     }
 
+    IEnumerator ByeAfterDelay(float time)
+    {
+        yield return new WaitForSeconds(time);
 
+        //GameLoader.gameOn = false;
 
+    }
 }
